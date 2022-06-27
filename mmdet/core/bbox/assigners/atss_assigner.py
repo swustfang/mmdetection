20e75b2c94739400ec6e9ddb9706520eff11d897
FZY <2788321369@qq.com>
2022-06-27 15:24:39 +0800

    删除之前加入到回归的分类分支，作为Dynamic ATSS第一版

--- mmdet/core/bbox/assigners/atss_assigner.py
+++ mmdet/core/bbox/assigners/atss_assigner.py
@@ -72,20 +72,20 @@ class ATSSAssigner(BaseAssigner):
         overlaps = self.iou_calculator(bboxes, gt_bboxes)
 
         # assign 0 by default
-        assigned_gt_inds = overlaps.new_full((num_bboxes, ),
+        assigned_gt_inds = overlaps.new_full((num_bboxes,),
                                              0,
                                              dtype=torch.long)
 
         if num_gt == 0 or num_bboxes == 0:
             # No ground truth or boxes, return empty assignment
-            max_overlaps = overlaps.new_zeros((num_bboxes, ))
+            max_overlaps = overlaps.new_zeros((num_bboxes,))
             if num_gt == 0:
                 # No truth, assign everything to background
                 assigned_gt_inds[:] = 0
             if gt_labels is None:
                 assigned_labels = None
             else:
-                assigned_labels = overlaps.new_full((num_bboxes, ),
+                assigned_labels = overlaps.new_full((num_bboxes,),
                                                     -1,
                                                     dtype=torch.long)
             return AssignResult(
@@ -167,7 +167,7 @@ class ATSSAssigner(BaseAssigner):
             max_overlaps != -INF] = argmax_overlaps[max_overlaps != -INF] + 1
 
         if gt_labels is not None:
-            assigned_labels = assigned_gt_inds.new_full((num_bboxes, ), -1)
+            assigned_labels = assigned_gt_inds.new_full((num_bboxes,), -1)
             pos_inds = torch.nonzero(
                 assigned_gt_inds > 0, as_tuple=False).squeeze()
             if pos_inds.numel() > 0:
